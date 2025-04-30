ba034682e71e23281766b6dd7fdef0e5d9870647
idembele70 <idembele70@gmail>
2025-04-30 11:51:04 +0200

    feat: initialize`addTodo` component
    Refs: #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddTodoComponent } from "./components/add-todo/add-todo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AddTodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
 
 }
