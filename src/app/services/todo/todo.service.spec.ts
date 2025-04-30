75e56d61034d6a045dda6ea831aeca4a28e20d99
idembele70 <idembele70@gmail>
2025-04-30 20:41:33 +0200

    test: implement addtodo TU

--- src/app/services/todo/todo.service.spec.ts
+++ src/app/services/todo/todo.service.spec.ts
@@ -1,16 +1,47 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TodoService } from './todo.service';
+import { Todo } from '../../models/todo';
 
 describe('TodoService', () => {
-  let service: TodoService;
+  let todoService: TodoService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(TodoService);
+    todoService = TestBed.inject(TodoService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(todoService).toBeTruthy();
   });
+
+  it('should add todo', () => {
+    const content = 'Test todo';
+    todoService.addTodo(content);
+    expect(todoService['_todos'].length).toBe(1);
+    expect(todoService['_todos'][0].content).toEqual(content);
+  })
+
+  it('should return true if todo exists', () => {
+    const content = 'Existing todo';
+    todoService.addTodo(content);
+    expect(todoService.todoExists(content)).toBeTrue()
+  })
+
+  it('should return false if todo does not exist', () => {
+    const content = 'unexisting todo'
+    expect(todoService.todoExists(content)).toBeFalse()
+  })
+
+  it('should emit todos', (done) => {
+    const content = 'todo to emit'
+    todoService.addTodo(content)
+    todoService.todoSubject.subscribe((todos: Todo[])=>{
+      const numberOfTodos = 1
+      expect(todos.length).toBe(numberOfTodos)
+      expect(todos[0].content).toBe(content)
+      done()
+    })
+    todoService.emitTodos();
+  })
 });
