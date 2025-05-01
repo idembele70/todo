403c4b27f65c344492bec58d73fa947d7854cd2e
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2025-05-01 14:26:03 +0200

    Feat: # 1 add a new todo (#2)
    * feat: initialize`addTodo` component
    
    Refs: #1
    
    * chore: add uuid to npm package
    
    * feat: init `todo-list`, `todo-row` & `todo.service`
    
    * test: implement addtodo TU
    
    * feat: remove static todo & add `todoExists` method
    
    Refs: #1
    
    * feat: add todo feature done
    
    Refs: #1
    
    * test: implement `add-todo` TU
    
    Refs: #1
    
    * refactor: format HTML and Ts files for consistency
    
    Refs: #1
    
    ---------
    
    Co-authored-by: idembele70 <idembele70@gmail>

---
+++ src/app/components/add-todo/add-todo.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Todo } from '../../models/todo';
+import { TodoService } from '../../services/todo/todo.service';
+
+@Component({
+  selector: 'app-add-todo',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './add-todo.component.html',
+  styleUrl: './add-todo.component.css'
+})
+export class AddTodoComponent implements OnInit, OnDestroy {
+  public newTodoText: string = '';
+  public todosSubscription: Subscription;
+  public todos: Todo[];
+  public disableAddBtn: boolean;
+  public disableAddInput: boolean;
+  constructor(private todoService: TodoService) {
+    this.todosSubscription = new Subscription();
+    this.todos = []
+    this.disableAddBtn = false;
+    this.disableAddInput = false;
+  }
+
+  ngOnInit(): void {
+      this.todosSubscription = this.todoService.todoSubject.subscribe(
+        {
+          next: (todos: Todo[])=>{
+            this.todos = todos
+          },
+          error: (err) => {
+              console.error(
+                "An error has occured when subscribing to todoSubject",
+                err
+              );
+              
+          },
+        },
+      );
+      this.todoService.emitTodos();
+  }
+
+  ngOnDestroy(): void {
+      this.todosSubscription.unsubscribe()
+  }
+
+  public onUpdateNewTodoText() {
+    const todoExist = this.todoService.todoExists(this.newTodoText)
+    if(todoExist) 
+      this.disableAddBtn = true
+    else if( this.disableAddBtn)
+      this.disableAddBtn = false
+  }
+
+  public onAddTodo() {
+    this.disableAddInput = true
+    this.disableAddBtn = true
+    this.todoService.addTodo(this.newTodoText)
+    this.newTodoText = ''
+    this.todoService.emitTodos()
+    this.disableAddInput = false
+    this.disableAddBtn = false
+  }
+}
---
+++ src/app/components/todo-list/todo-list.component.ts
@@ -0,0 +1,44 @@
+import { NgForOf } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Todo } from '../../models/todo';
+import { TodoService } from '../../services/todo/todo.service';
+import { TodoRowComponent } from "../todo-row/todo-row.component";
+
+@Component({
+  selector: 'app-todo-list',
+  standalone: true,
+  imports: [TodoRowComponent, NgForOf],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.css'
+})
+export class TodoListComponent implements OnInit, OnDestroy {
+  public todosSubscription: Subscription;
+  public todos: Todo[];
+
+  constructor(private todoService: TodoService) {
+    this.todosSubscription = new Subscription();
+    this.todos = []
+  }
+
+  ngOnInit(): void {
+    this.todosSubscription = this.todoService.todoSubject.subscribe(
+      {
+        next: (todos: Todo[]) => {
+          this.todos = todos
+        },
+        error: (err) => {
+          console.error(
+            'An error has occured when subscribing to todoSubject',
+            err
+          );
+        }
+      }
+    )
+    this.todoService.emitTodos()
+  }
+
+  ngOnDestroy(): void {
+    this.todosSubscription.unsubscribe()
+  }
+}
