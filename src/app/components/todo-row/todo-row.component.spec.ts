beeafab7d84e5a022e47e74005a8928ef5ce848c
idembele70 <idembele70@gmail>
2025-05-02 02:03:25 +0200

    feat(todo-row): implement toggle todo as completed
    Refs: #2

--- src/app/components/todo-row/todo-row.component.spec.ts
+++ src/app/components/todo-row/todo-row.component.spec.ts
@@ -1,9 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { v1 as id } from 'uuid';
+import { Todo } from '../../models/todo';
 import { TodoRowComponent } from './todo-row.component';
-
 describe('TodoRowComponent', () => {
   let component: TodoRowComponent;
   let fixture: ComponentFixture<TodoRowComponent>;
+  let toggleSpy: jasmine.Spy<(id: string) => boolean>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,9 +16,65 @@ describe('TodoRowComponent', () => {
     fixture = TestBed.createComponent(TodoRowComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    component.disableCheckbox = false;
+    component.disableCloseBtn = false;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should mark a todo as done', fakeAsync(() => {
+    toggleSpy = spyOn(component['todoService'], 'toggleCompletedTodo').and.callThrough();
+    const todo: Todo = {
+      id: id(),
+      content: 'toggle todo content',
+      createdAt: new Date(),
+      done: false
+    };
+
+    component.todo = todo;
+    component['todoService']['_todos'] = [todo];
+
+    component.onComplete();
+
+    expect(component.disableCheckbox).toBeTrue();
+    expect(component.disableCloseBtn).toBeTrue();
+
+    tick(500);
+
+    expect(component.disableCheckbox).toBeFalse();
+    expect(component.disableCloseBtn).toBeFalse();
+    expect(toggleSpy).toHaveBeenCalledWith(component.todo.id);
+    expect(component.todo.done).toBeTrue();
+    expect(component['todoService']['_todos'][0].done).toBeTrue();
+  }));
+
+  it('should unmark a completed todo', fakeAsync(() => {
+    toggleSpy = spyOn(component['todoService'], 'toggleCompletedTodo').and.callThrough();
+
+    const todo: Todo = {
+      id: id(),
+      content: 'undone todo',
+      createdAt: new Date(),
+      done: true
+    };
+
+    component.todo = todo;
+    component['todoService']['_todos'] = [todo];
+
+    component.onComplete();
+
+    expect(component.disableCheckbox).toBeTrue();
+    expect(component.disableCloseBtn).toBeTrue();
+
+    tick(500);
+
+    expect(component.disableCheckbox).toBeFalse();
+    expect(component.disableCloseBtn).toBeFalse();
+    expect(component.todo.done).toBeFalse();
+    expect(component['todoService']['_todos'][0].done).toBeFalse();
+    expect(toggleSpy).toHaveBeenCalledWith(component.todo.id);
+  }));
 });
--- src/app/components/todo-row/todo-row.component.ts
+++ src/app/components/todo-row/todo-row.component.ts
@@ -1,23 +1,38 @@
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Todo } from '../../models/todo';
+import { TodoService } from '../../services/todo/todo.service';
 
 @Component({
   selector: 'app-todo-row',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, NgClass],
   templateUrl: './todo-row.component.html',
   styleUrl: './todo-row.component.css'
 })
 export class TodoRowComponent {
   @Input() todo: Todo;
-
-  constructor() {
+  disableCheckbox: boolean;
+  disableCloseBtn: boolean;
+  constructor(private todoService: TodoService) {
     this.todo = {
       id: "id-default-xyz",
       content: 'content-default',
       createdAt: new Date(),
       done: false
-    }
+    };
+    this.disableCheckbox = false;
+    this.disableCloseBtn = false;
+  }
+
+  onComplete() {
+    this.disableCheckbox = true;
+    this.disableCloseBtn = true;
+    // Simulate server request/response time.
+    setTimeout(() => {
+      this.todoService.toggleCompletedTodo(this.todo.id);
+      this.disableCheckbox = false;
+      this.disableCloseBtn = false;
+    }, 500);
   }
 }
