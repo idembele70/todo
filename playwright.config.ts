40b776387368eed08e7ee30bc7b3a01226cbd7ef
idembele70 <idembele70@gmail>
2025-05-06 15:46:37 +0200

    chore: add @estruyf/github-actions-reporter for enhanced E2E GitHub Actions reporting
    Refs: #12

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,4 @@
+import type { GitHubActionOptions } from '@estruyf/github-actions-reporter';
 import { defineConfig, devices } from '@playwright/test';
 
 /**
@@ -22,7 +23,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env['CI'] ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env['CI'] ? 'github' : 'html',
+  reporter: process.env['CI'] ? [['list'], ['@estruyf/github-actions-reporter', <GitHubActionOptions>{
+    title: 'E2E Test Report',
+    useDetails: true,
+    showError: true
+  }]] : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
