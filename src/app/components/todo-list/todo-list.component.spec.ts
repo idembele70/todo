f61889e69534d9c55816920308789c6ae6f39269
idembele70 <idembele70@gmail>
2025-05-12 01:00:12 +0200

    test: fix broken tests to fit the new behavior
    Refs: #19

--- src/app/components/todo-list/todo-list.component.spec.ts
+++ src/app/components/todo-list/todo-list.component.spec.ts
@@ -1,21 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { TodoListComponent } from './todo-list.component';
-import { TodoService } from '../../services/todo/todo.service';
-import { Subject } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { of, Subscription } from 'rxjs';
 import { Todo } from '../../models/todo.model';
+import { TodoService } from '../../services/todo/todo.service';
+import { TodoListComponent } from './todo-list.component';
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
   let fixture: ComponentFixture<TodoListComponent>;
-  let todoSubject: Subject<Todo[]>;
-  let todoServiceMock: jasmine.SpyObj<TodoService>;
+  let todoService: TodoService;
+
   beforeEach(async () => {
-    todoSubject = new Subject<Todo[]>();
-    todoServiceMock = jasmine.createSpyObj('todoService', ['emitTodos'], { todoSubject });
+    todoService = new TodoService();
+    const activatedRouteMock = {
+      url: of([{ path: 'active' }]),
+    };
+
     await TestBed.configureTestingModule({
       imports: [TodoListComponent],
-      providers: [{ provide: TodoService, useValue: todoServiceMock }],
+      providers: [
+        { provide: TodoService, useValue: todoService },
+        { provide: ActivatedRoute, useValue: activatedRouteMock },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TodoListComponent);
@@ -27,25 +33,45 @@ describe('TodoListComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should call subscribe on init', () => {
-    const subscribeSpy = spyOn(todoServiceMock.todoSubject, 'subscribe').and.callThrough();
-
+  it('should call path subscribe on init', () => {
+    spyOn(todoService, 'emitPath').and.callThrough();
     component.ngOnInit();
-
-    expect(subscribeSpy).toHaveBeenCalled();
+    expect(todoService.emitPath).toHaveBeenCalledOnceWith('active');
   });
 
-  it('should call unsubscribe from todoSubscription on detroy', () => {
-    const unsubscribeSpy = spyOn(component.todosSubscription, 'unsubscribe');
+  it('should call unsubscribe from pathSubscription on destroy', () => {
+    const dummySub = jasmine.createSpyObj<Subscription>('Subscription', ['unsubscribe']);
+    (component as unknown as { pathSubscription: Subscription }).pathSubscription = dummySub;
 
     component.ngOnDestroy();
 
-    expect(unsubscribeSpy).toHaveBeenCalled();
+    expect(dummySub.unsubscribe).toHaveBeenCalled();
   });
 
-  it('should call emitTodos on onDeleteOneTodo()', () => {
+  it('should render todos from the observable', () => {
+    const todos: Todo[] = [
+      {
+        id: '12',
+        content: 'Test todo',
+        done: false,
+        createdAt: new Date(),
+      },
+    ];
+    const expectedTodoLength = todos.length;
+    const expectedTodoContent = 'Test todo';
+
+    todoService.todoSubject.next(todos);
+
+    fixture.detectChanges();
+
+    const todoElements = fixture.nativeElement.querySelectorAll('li');
+    expect(todoElements.length).toBe(expectedTodoLength);
+    expect(todoElements[0]?.textContent).toContain(expectedTodoContent);
+  });
+
+  xit('should call emitTodos on onDeleteOneTodo()', () => {
     component.onDeleteOneTodo();
 
-    expect(todoServiceMock.emitTodos).toHaveBeenCalled();
+    expect(todoService.emitTodos).toHaveBeenCalled();
   });
 });
