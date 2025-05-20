7c3e3bf4503a533b76b4c4484765edfada9751b7
idembele70 <idembele70@gmail>
2025-05-20 20:01:59 +0200

    test: add mock `ActivatedRoute` provider for `HomeComponent` tests
    Refs: #19

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { ActivatedRoute, UrlSegment } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -9,6 +10,14 @@ describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomeComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            url: of([{ path: '' } as UrlSegment]),
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
