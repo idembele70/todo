390b2c32cb119fbb7c533812f1a55b55e618511f
idembele70 <idembele70@gmail>
2025-05-22 16:30:00 +0200

    test: refactor TodoListPage POM to improve structure and add new methods & properties for todo management
    - Update & rename `urlPattern` to `urlRegExp`.
    - Rename `inputField` to `addTodoInputField`.
    - Rename `todoRow` to `todoRows`.
    - Add `editModeInput` property.
    - Refactor `fillInput`, `assertEmptyInputValue`, `pressEnter`  & `assertTodoCount` methods.
    - Add `addTodo`, `getTodoRowByContent`, `hoverTodoRow`,  `fillTodoEditInput`, `blurTodoEditInput`, `assertEditModeInputIsHidden`, `assertTodoRowCheckboxIsVisible`, `assertTodoRowDeleteButtonIsVisible`, `editTodoRowContent`, `assertTodoRowIsVisible`, `gotToAllTodosPage`, `goToCompletedTodosPage`, `goToActiveTodosPage`, `completeTodo` & `assertPageURL` methods.
    
    Refs: #19

--- e2e/pages/todoList.page.ts
+++ e2e/pages/todoList.page.ts
@@ -2,23 +2,37 @@ import { expect, Locator, Page } from '@playwright/test';
 
 export default class TodoListPage {
   private readonly page: Page;
-  public readonly urlPattern: RegExp;
+  public readonly urlRegExp: RegExp;
 
-  public inputField: Locator;
-  public addButton: Locator;
+  public readonly baseURL: string;
+  public readonly allTodosPageURL: string;
+  public readonly completedTodosPageURL: string;
+  public readonly activeTodosPageURL: string;
 
-  public todoList: Locator;
-  public todoRow: Locator;
+  public readonly addTodoInputField: Locator;
+  public readonly addButton: Locator;
+
+  public readonly todoList: Locator;
+  public readonly todoRows: Locator;
+
+  public readonly editModeInput: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    this.urlPattern = /.*#\/(all|completed|active)$/;
 
-    this.inputField = this.page.locator('input[placeholder="What needs to be done?"]');
+    this.baseURL = 'home/';
+    this.urlRegExp = new RegExp(`.*/${this.baseURL}(all|completed|active)$`);
+    this.allTodosPageURL = `${this.baseURL}all`;
+    this.completedTodosPageURL = `${this.baseURL}completed`;
+    this.activeTodosPageURL = `${this.baseURL}active`;
+
+    this.addTodoInputField = this.page.locator('input[placeholder="What needs to be done?"]');
     this.addButton = this.page.getByRole('button', { name: 'Add', exact: true });
 
     this.todoList = this.page.locator('ul');
-    this.todoRow = this.page.locator('li');
+    this.todoRows = this.page.locator('li');
+
+    this.editModeInput = this.page.getByTestId('edit-mode-input');
   }
 
   /**
@@ -27,7 +41,7 @@ export default class TodoListPage {
    * @param {string} text - The text to type into the input field.
    */
   public async fillInput(text: string) {
-    await this.inputField.fill(text);
+    await this.addTodoInputField.fill(text);
   }
 
   /**
@@ -35,7 +49,7 @@ export default class TodoListPage {
    */
   public async assertEmptyInputValue() {
     const expectedEmptyValue = '';
-    await expect(this.inputField).toHaveValue(expectedEmptyValue);
+    await expect(this.addTodoInputField).toHaveValue(expectedEmptyValue);
   }
 
   /**
@@ -47,16 +61,16 @@ export default class TodoListPage {
   /**
    * Asserts that the add button is disabled.
    */
-
   public async assertAddButtonIsDisabled() {
     await expect(this.addButton).toBeDisabled();
   }
 
   /**
-   * Press the "Enter" key to submit a new todo item.
+   * Presses the "Enter" key on the specified locator.
+   * @param {Locator} locator - The element locator to receive the Enter key press.
    */
-  public async pressEnter() {
-    await this.inputField.press('Enter');
+  public async pressEnter(locator: Locator) {
+    await locator.press('Enter');
   }
 
   /**
@@ -65,6 +79,137 @@ export default class TodoListPage {
    * @param {number} expectedCount - The expected number of todos.
    */
   public async assertTodoCount(expectedCount: number) {
-    await expect(this.todoRow).toHaveCount(expectedCount);
+    await expect(this.todoRows).toHaveCount(expectedCount);
+  }
+
+  /**
+   * Adds a new todo item and ensures it's visible in the list.
+   *
+   * @param {string} content - The text content of the todo to be added.
+   */
+  public async addTodo(content: string) {
+    await this.fillInput(content);
+    await this.pressEnter(this.addTodoInputField);
+
+    const createdTodoRows = this.todoRows.getByText(content, { exact: true });
+    await createdTodoRows.waitFor({ state: 'visible' });
+  }
+
+  /**
+   * awaits a todo row by its exact text content.
+   *
+   * @param {string} content - The exact text of the todo item.
+   * @awaits {Locator} - The locator of the todo row containing the given content.
+   */
+  public getTodoRowByContent(content: string): Locator {
+    return this.todoRows.filter({ hasText: content });
+  }
+
+  /**
+   * Hover over the the given todo row.
+   * @param {Locator} todoRow - The locator of the todo row to hover.
+   */
+  public async hoverTodoRow(todoRow: Locator) {
+    await todoRow.hover();
+  }
+
+  /**
+   * Fills the input field for editing a todo item with the provided content.
+   * @param {string} content - The new content to set in the edit input.
+   */
+  public async fillTodoEditInput(content: string) {
+    await this.editModeInput.fill(content);
+  }
+
+  /**
+   * Triggers a blur event on the edit mode input.
+   */
+  public async blurTodoEditInput() {
+    await this.editModeInput.blur();
+  }
+
+  /**
+   * Asserts that the edit mode input is hidden.
+   */
+  public async assertEditModeInputIsHidden() {
+    await expect(this.editModeInput).toBeHidden();
+  }
+
+  /**
+   * Asserts that the checkbox of the given todo row is visible.
+   * @param {Locator} todoRow - The locator of the todo row.
+   */
+  public async assertTodoRowCheckboxIsVisible(todoRow: Locator) {
+    const checkbox = todoRow.getByRole('checkbox');
+    await expect(checkbox).toBeVisible();
+  }
+
+  /**
+   * Asserts that the delete button of the given todo row is visible.
+   * @param {Locator} todoRow - The locator of the todo row.
+   */
+  public async assertTodoRowDeleteButtonIsVisible(todoRow: Locator) {
+    const closeButton = todoRow.locator('.btn-close');
+    await expect(closeButton).toBeVisible();
+  }
+
+  /**
+   * Edits the content of the given todo row.
+   * @param {Locator} todoRow - The locator of the todo row to edit.
+   * @param {string} content - The new content to set.
+   */
+  public async editTodoRowContent(todoRow: Locator, content: string) {
+    await todoRow.dblclick();
+
+    await this.editModeInput.fill(content);
+    await this.editModeInput.blur();
+
+    const updatedTodoRow = this.todoRows.filter({ hasText: content });
+    await updatedTodoRow.waitFor({ state: 'visible' });
+  }
+
+  /**
+   * Asserts that the given todo row is visible.
+   * @param {Locator} todoRow  - The locator of the todo row.
+   */
+  public async assertTodoRowIsVisible(todoRow: Locator) {
+    await expect(todoRow).toBeVisible();
+  }
+
+  /**
+   * Redirects to the page where all todos are visible.
+   */
+  public async goToAllTodosPage() {
+    await this.page.goto(this.allTodosPageURL);
+  }
+
+  /**
+   * Redirects to the page where completed todos are visible.
+   */
+  public async goToCompletedTodosPage() {
+    await this.page.goto(this.completedTodosPageURL);
+  }
+
+  /**
+   * Redirects to the page where active todos are visible
+   */
+  public async goToActiveTodosPage() {
+    await this.page.goto(this.activeTodosPageURL);
+  }
+
+  /**
+   * Checks the checkbox pf the given todo row.
+   * @param {Locator} todoRow - The locator of the todo row to complete.
+   */
+  public async completeTodo(todoRow: Locator) {
+    const checkbox = todoRow.getByRole('checkbox');
+    await checkbox.check();
+  }
+
+  /**
+   * Asserts that the current page URL macthes the expected pattern.
+   */
+  public async assertPageURL() {
+    await expect(this.page).toHaveURL(this.urlRegExp);
   }
 }
