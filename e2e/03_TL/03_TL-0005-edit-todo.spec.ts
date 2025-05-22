f8230ebfe85be58bc1ce08c93f1cad192764b1f0
idembele70 <idembele70@gmail>
2025-05-22 16:40:53 +0200

    test(TL-0005): write e2e test for editing todos across all home page
    Refs: #19

---
+++ e2e/03_TL/03_TL-0005-edit-todo.spec.ts
@@ -0,0 +1,84 @@
+import test from '@playwright/test';
+import TodoListPage from '../pages/todoList.page';
+
+test.describe('TodoList - Edit Todo', { tag: '@todoListPage' }, () => {
+  const TODO_CONTENT = 'Todo content';
+  const EDITED_CONTENT = 'Content edited';
+
+  test.describe('All Todos page', () => {
+    let todoListPage: TodoListPage;
+    test.beforeEach(async ({ page }) => {
+      todoListPage = new TodoListPage(page);
+      await todoListPage.goToAllTodosPage();
+
+      await todoListPage.addTodo(TODO_CONTENT);
+    });
+
+    test('should allow the user to edit a todo content and save by pressing "Enter" key', async () => {
+      let todoRow = todoListPage.getTodoRowByContent(TODO_CONTENT);
+      await todoRow.dblclick();
+
+      await todoListPage.fillTodoEditInput(EDITED_CONTENT);
+      await todoListPage.pressEnter(todoListPage.editModeInput);
+
+      todoRow = todoListPage.getTodoRowByContent(EDITED_CONTENT);
+      await todoListPage.assertTodoRowCheckboxIsVisible(todoRow);
+      await todoListPage.hoverTodoRow(todoRow);
+      await todoListPage.assertTodoRowDeleteButtonIsVisible(todoRow);
+    });
+
+    test('should allow user to delete a todo when input is cleared', async () => {
+      const todoRow = todoListPage.getTodoRowByContent(TODO_CONTENT);
+      await todoRow.dblclick();
+
+      const emptyContent = '';
+      await todoListPage.fillTodoEditInput(emptyContent);
+
+      await todoListPage.blurTodoEditInput();
+
+      await todoListPage.assertEditModeInputIsHidden();
+
+      const expectedCount = 0;
+      await todoListPage.assertTodoCount(expectedCount);
+    });
+  });
+
+  test.describe('Active Todos page', () => {
+    test('should edit todo in active todos page', async ({ page }) => {
+      const todoListPage = new TodoListPage(page);
+
+      await todoListPage.goToActiveTodosPage();
+      await todoListPage.assertPageURL();
+      await todoListPage.addTodo(TODO_CONTENT);
+
+      let todoRow = todoListPage.getTodoRowByContent(TODO_CONTENT);
+      await todoListPage.editTodoRowContent(todoRow, EDITED_CONTENT);
+
+      todoRow = todoListPage.getTodoRowByContent(EDITED_CONTENT);
+      await todoListPage.assertTodoRowIsVisible(todoRow);
+    });
+  });
+
+  test.describe('Completed Todos page', () => {
+    test.fixme(true, 'no data persistance between navigation; cannot assert todo edit on completed page.');
+
+    test('should edit todo in completed todos page', async ({ page }) => {
+      const todoListPage = new TodoListPage(page);
+
+      await todoListPage.goToCompletedTodosPage();
+      await todoListPage.assertPageURL();
+      await todoListPage.addTodo(TODO_CONTENT);
+
+      let todoRow = todoListPage.getTodoRowByContent(TODO_CONTENT);
+
+      await todoListPage.completeTodo(todoRow);
+
+      await todoListPage.goToActiveTodosPage();
+
+      await todoListPage.editTodoRowContent(todoRow, EDITED_CONTENT);
+
+      todoRow = todoListPage.getTodoRowByContent(EDITED_CONTENT);
+      await todoListPage.assertTodoRowIsVisible(todoRow);
+    });
+  });
+});
