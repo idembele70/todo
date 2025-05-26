15a494cf9e0af9cacf4586052a771a1eb4a9d841
idembele70 <idembele70@gmail>
2025-05-27 00:29:17 +0200

    test(todo-view): add unit test
    Refs: #19

--- src/app/core/utils/todo.factory.ts
+++ src/app/core/utils/todo.factory.ts
@@ -1,3 +1,4 @@
+import { fakerFR } from '@faker-js/faker';
 import { Todo } from '../../models/todo.model';
 
 export const createEmptyTodo = (): Todo => {
@@ -7,4 +8,17 @@ export const createEmptyTodo = (): Todo => {
     createdAt: new Date(),
     done: false,
   };
+};
+
+export const createRandomTodo = (): Todo => {
+  return {
+    id: fakerFR.string.uuid(),
+    content: fakerFR.lorem.sentence(2),
+    createdAt: fakerFR.date.recent(),
+    done: false,
+  };
+};
+
+export const createRandomContentTodo = (): string => {
+  return fakerFR.lorem.sentence(2);
 };
