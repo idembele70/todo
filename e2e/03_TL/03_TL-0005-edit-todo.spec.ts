dfcc800226cdb8882321e224bda24b45df32f165
idembele70 <idembele70@gmail>
2025-05-31 15:51:26 +0200

    test(TL-0005): fix  skipped test, refactor logic, update import & content creation
    Refs: #21

--- e2e/03_TL/03_TL-0005-edit-todo.spec.ts
+++ e2e/03_TL/03_TL-0005-edit-todo.spec.ts
@@ -1,9 +1,11 @@
-import test from '@playwright/test';
-import TodoListPage from '../pages/todo-list.page';
+import test, { Page } from '@playwright/test';
+import TodoListPage from '@pw-pages/todo-list.page';
+import HeaderComponent from '@pw-pages/components/header.component';
+import { generateRandomContent } from '@pw-helpers/todo.helpers';
 
 test.describe('TodoList - Edit Todo', { tag: '@TodoListPage' }, () => {
-  const TODO_CONTENT = 'Todo content';
-  const EDITED_CONTENT = 'Content edited';
+  const TODO_CONTENT = generateRandomContent();
+  const EDITED_CONTENT = generateRandomContent();
 
   test.describe('All Todos page', () => {
     let todoListPage: TodoListPage;
@@ -69,20 +71,28 @@ test.describe('TodoList - Edit Todo', { tag: '@TodoListPage' }, () => {
   });
 
   test.describe('Completed Todos page', () => {
-    test.fixme(true, 'no data persistence between navigation; cannot assert todo edit on completed page.');
+    let page: Page;
+    let todoListPage: TodoListPage;
+    let headerComponent: HeaderComponent;
 
-    test('should edit todo in completed todos page', async ({ page }) => {
-      const todoListPage = new TodoListPage(page);
+    test.beforeAll(async ({ browser }) => {
+      page = await browser.newPage();
+      todoListPage = new TodoListPage(page);
+      headerComponent = new HeaderComponent(page);
 
-      await todoListPage.goToCompletedTodosPage();
-      await todoListPage.assertPageURL();
+      await todoListPage.goToAllTodosPage();
       await todoListPage.addTodo(TODO_CONTENT);
+    });
 
+    test.afterAll(async () => {
+      await page.close();
+    });
+
+    test('should edit todo in completed todos page', async () => {
       let todoRow = todoListPage.getTodoRowByContent(TODO_CONTENT);
-
       await todoListPage.completeTodo(todoRow);
 
-      await todoListPage.goToActiveTodosPage();
+      await headerComponent.navigateToCompleteTodosPage();
 
       await todoListPage.editTodoRowContent(todoRow, EDITED_CONTENT);
 
