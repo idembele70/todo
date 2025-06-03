f7a724fbd1cdd4ddf2cbe826f4fdc1703459550d
idembele70 <idembele70@gmail>
2025-06-04 01:46:51 +0200

    feat(app): generate not found view
    Refs: #25

---
+++ src/app/pages/four-oh-four/four-oh-four.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-four-oh-four',
+  standalone: true,
+  imports: [],
+  templateUrl: './four-oh-four.component.html',
+  styleUrl: './four-oh-four.component.css'
+})
+export class FourOhFourComponent {
+
+}
