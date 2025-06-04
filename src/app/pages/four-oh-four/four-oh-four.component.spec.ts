f8f4f787e2f256726ff9b22555f1cbfd149c7951
idembele70 <idembele70@gmail>
2025-06-04 15:49:22 +0200

    test(UT): add unit test for four-oh-four component
    Refs: #25

--- src/app/pages/four-oh-four/four-oh-four.component.spec.ts
+++ src/app/pages/four-oh-four/four-oh-four.component.spec.ts
@@ -1,17 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FourOhFourComponent } from './four-oh-four.component';
 
 describe('FourOhFourComponent', () => {
   let component: FourOhFourComponent;
   let fixture: ComponentFixture<FourOhFourComponent>;
 
+  const TITLE = '404';
+  const SUB_TITLE = 'Page Not Found';
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FourOhFourComponent]
-    })
-    .compileComponents();
-    
+      imports: [FourOhFourComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(FourOhFourComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +20,17 @@ describe('FourOhFourComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should verify title is ${TITLE}`, () => {
+    const pageTitleEl = fixture.nativeElement.querySelector('h2') as HTMLHeadingElement;
+
+    expect(pageTitleEl.textContent).toEqual(TITLE);
+  });
+
+  it(`should verify sub-title is ${SUB_TITLE}`, () => {
+    const subTitleEl = fixture.nativeElement.querySelector('h5') as HTMLHeadingElement;
+
+    expect(subTitleEl.textContent).toEqual(SUB_TITLE);
   });
 });
