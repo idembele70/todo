67d9f10ce03a4ae8096d914bcf74fe76957de50c
idembele70 <idembele70@gmail>
2025-06-07 15:00:24 +0200

    test(footer): add e2e test for TL-0010 clear completed todos
    Refs: #31

--- e2e/pages/components/footer.component.ts
+++ e2e/pages/components/footer.component.ts
@@ -4,4 +4,13 @@ export default class FooterComponent {
   constructor(private page: Page) {}
 
   readonly container: Locator = this.page.locator('footer');
+  readonly todoCount: Locator = this.page.getByTestId('todo-count');
+  readonly clearCompletedButton: Locator = this.page.getByRole('button', { name: 'Clear completed' });
+
+  /**
+   * Clicks the "Clear completed" button to delete all completed todos from the UI.
+   */
+  public async clickClearCompletedButton() {
+    await this.clearCompletedButton.click();
+  }
 }
